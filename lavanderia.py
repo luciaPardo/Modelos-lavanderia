e0d87730c33cca8f671a3b80f0552368f271284e
luciapardo <lpardo@fi.uba.ar>
2022-08-29 20:21:19 -0300

    primer acercamiento

--- lavanderia.py
+++ lavanderia.py
@@ -1,21 +1,51 @@
 def menor_tiempo_prendas(archivo):
     """ """
-    tiempo = []
+    tiempo = {}
     params = []
-    incom = []
+    incom = {}
     with open(archivo, "r") as lav:
         for linea in lav:
             if linea[0] == "p":  # parametros
-                params.append(linea.split()[1::])
-            else if linea[0] == "e":  # incompatibilidad
-                incom.append(linea.split()[1::])
-            else if linea[0] == "n":  # tiempo
-                tiempo.append(linea.split()[1::])
-
-    tiempo_lavanderia(params, incom, tiempo)
+                params = linea.split()[1::]
+            elif linea[0] == "e":  # incompatibilidad
+                lin = linea.split()[1::]
+                incom[lin[0]] = incom.get(lin[0], []) + [lin[1]]
+            elif linea[0] == "n":  # tiempo
+                lin = linea.split()[1::]
+                tiempo[lin[0]] = lin[1]
+    tiempo_lavanderia(params, tiempo, incom)
 
 
 def tiempo_lavanderia(parametros, tiempo, incom):
+    suma = 0
+    incompat_tot = []
+    a_lavar = []
+    lavados = []
+    while len(lavados) <= int(parametros[1]):
+        for prenda in incom:
+            if len(a_lavar) == 0:
+                a_lavar.append(prenda)
+            elif prenda not in a_lavar and not es_incompatible(a_lavar, incom, prenda):
+                a_lavar.append(prenda)
+        # print(a_lavar)
+        a_sumar = 0
+        for elem in a_lavar:
+            if int(tiempo[elem]) > a_sumar:
+                a_sumar = int(tiempo[elem])
+        suma += a_sumar
+        lavados += a_lavar
+        a_lavar = []
+    print(suma)
+    return
 
-    for _ in range(parametros[1]):
-        # busqueda binaria????
+
+def es_incompatible(a_lavar, incom, por_agregar):
+    print(a_lavar)
+    for prenda in a_lavar:
+        print(incom[por_agregar])
+        if por_agregar in a_lavar or prenda in incom[por_agregar]:
+            return True
+    return False
+
+
+menor_tiempo_prendas("primer_problema.txt")
