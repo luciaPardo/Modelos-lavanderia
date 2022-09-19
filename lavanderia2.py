0d384e6703263d86244c3e8d7ba81ca44c28c390
luciapardo <lpardo@fi.uba.ar>
2022-09-19 01:30:32 -0300

    ordeno y empiezo a hacer shuffle hasta conseguir mejor puntaje

--- lavanderia2.py
+++ lavanderia2.py
@@ -1,9 +1,12 @@
 import numpy as np
+import random
+# list(itertools.permutations([1, 2, 3]))
 
 
-def menor_tiempo_prendas(archivo):
+def parsear_input(archivo):
     """ """
     tiempo = []
+    params = []
     incom = {}
     with open(archivo, "r") as lav:
         for linea in lav:
@@ -15,47 +18,40 @@ def menor_tiempo_prendas(archivo):
             elif linea[0] == "n":  # tiempo
                 lin = list(map(int, linea.split()[1::]))
                 tiempo.append((lin[1], lin[0]))
-        tiempos_max = sorted(tiempo, reverse=True)
-        tiempo_lavanderia(incom, tiempos_max)
+        return incom, sorted(tiempo, reverse=True)
 
 
 def tiempo_lavanderia(incom, tiempos_max):
     suma = 0
     a_lavar = []
-    lavados = []
     soluc = {}
     lavado_nro = 1
+    print(tiempos_max)
+    mezclado = random.shuffle(tiempos_max)
     while len(tiempos_max):
         for mayor_tiempo in tiempos_max:
             prenda = mayor_tiempo[1]
             if len(a_lavar) == 0:
                 a_lavar.append(mayor_tiempo)
+                suma += mayor_tiempo[0]
             elif not es_incompatible(a_lavar, incom, prenda):
                 a_lavar.append(mayor_tiempo)
-        lavados += a_lavar
-        suma = pasar_a_solucion(a_lavar, suma, incom,
-                                soluc, lavado_nro, tiempos_max)
+        pasar_a_solucion(a_lavar, soluc, lavado_nro, tiempos_max)
         lavado_nro += 1
         a_lavar = []
-    print(suma)
+    # print(suma)
     parsear_output(soluc)
-    return
-
-
-def pasar_a_solucion(a_lavar, suma, incom, soluc, lavado_nro, tiempos_max):
-    a_sumar = 0
-    for tiempo, elem in a_lavar:
-        if tiempo > a_sumar:
-            a_sumar = tiempo
-        soluc[elem] = lavado_nro
-        incom.pop(elem, None)
-        tiempos_max.remove((tiempo, elem))
-    suma += a_sumar
     return suma
 
 
+def pasar_a_solucion(a_lavar, soluc, lavado_nro, tiempos_max):
+    for tiempo, elem in a_lavar:
+        soluc[elem] = lavado_nro
+        tiempos_max.remove((tiempo, elem))
+
+
 def parsear_output(soluc):
-    with open("entrega_2.txt", "w") as entrega:
+    with open("entrega_5.txt", "w") as entrega:
         for key, value in soluc.items():
             entrega.write(f"{key} {value}\n")
 
@@ -64,12 +60,23 @@ def es_incompatible(a_lavar, incom, por_agregar):
     for _, prenda in a_lavar:
         # Esto lo hago porque no estan hechas las reciprocas en el archivo con las incompatibilidades
         if por_agregar in incom:
-            if por_agregar in a_lavar or prenda in incom[por_agregar]:
+            if prenda in incom[por_agregar]:
                 return True
         if prenda in incom:
-            if por_agregar in a_lavar or por_agregar in incom[prenda]:
+            if por_agregar in incom[prenda]:
                 return True
     return False
 
 
-menor_tiempo_prendas("segundo_problema.txt")
+def main():
+    suma = 4000
+    soluc = {}
+
+    while (suma > 300):
+        incom, tiempos_max = parsear_input("segundo_problema.txt")
+        suma = tiempo_lavanderia(incom, tiempos_max)
+
+    print(suma)
+
+
+main()
